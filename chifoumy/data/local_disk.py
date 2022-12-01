75e76764a7cc6b0645ab7f48de99d378f8c219a1
Stephanie Gentil <gentil.stephanie@gmail.com>
2022-12-02 00:32:20 +0100

    packaging get data from png to csv

--- chifoumy/data/local_disk.py
+++ chifoumy/data/local_disk.py
@@ -8,11 +8,14 @@ import cv2
 import mediapipe as mp
 import pandas as pd
 
-def from_png_to_df(hand: str, verbose=True, save=True) -> pd.DataFrame:
+def from_png_to_csv(hand: str, verbose=True):
     """
-    return a dataframe of the coordinate, from png dataset, from local disk
-    if save=True, save the df in a csv file
+    return a dataframe with the coordinate of one gesture, from png files,
+    stored in a directory with the name of the gesture, from local disk.
+
+    hand: str > shape/gesture (rock, paper, scissors)
     """
+
     path = os.path.join(os.path.expanduser(LOCAL_DATA_PATH),hand)
     hand_image_files = []
 
@@ -28,26 +31,87 @@ def from_png_to_df(hand: str, verbose=True, save=True) -> pd.DataFrame:
     hands_list = []
 
     with mp_hands.Hands(static_image_mode=True,
-                        max_num_hands=2,
+                        max_num_hands=1,
                         min_detection_confidence=0.5) as hands:
+
         for idx, file in enumerate(hand_image_files):
 
-            # image = cv2.flip(cv2.imread(file), 1)
-            image = cv2.imread(file)
-            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            image_vanilla = cv2.imread(file)
+            image_flip = cv2.flip(cv2.imread(file), 1)
+            image_up = cv2.rotate(cv2.imread(file), cv2.ROTATE_90_COUNTERCLOCKWISE)
+            image_down = cv2.rotate(cv2.imread(file), cv2.ROTATE_90_CLOCKWISE)
 
-            if not results.multi_hand_landmarks:
+            results_vanilla = hands.process(cv2.cvtColor(image_vanilla, cv2.COLOR_BGR2RGB))
+            results_flip = hands.process(cv2.cvtColor(image_flip, cv2.COLOR_BGR2RGB))
+            results_up = hands.process(cv2.cvtColor(image_up, cv2.COLOR_BGR2RGB))
+            results_down = hands.process(cv2.cvtColor(image_down, cv2.COLOR_BGR2RGB))
+
+            if not results_vanilla.multi_hand_landmarks or not results_flip.multi_hand_landmarks \
+               or not results_up.multi_hand_landmarks or not results_down.multi_hand_landmarks:
+                if verbose:
+                   print(Fore.YELLOW + f"{idx} {file}: no multi_hand_landmarks found" + Style.RESET_ALL)
+
                 continue
 
-            for hand_landmarks in results.multi_hand_landmarks:
-                fingers = {}
+            # if verbose:
+            #        print(Fore.BLUE + f"hand {idx} landmarking" + Style.RESET_ALL)
+
+            for hand_landmarks in results_vanilla.multi_hand_landmarks:
+
+                fingers = {
+                    'filename' : file
+                }
+
                 for i, finger in enumerate(hand_landmarks.landmark, start=1):
                     fingers[f'{i}x'] = (finger.x)
                     fingers[f'{i}y'] = (finger.y)
+                    fingers[f'{i}z'] = (finger.z)
+
                 hands_list.append(fingers)
+
+            for hand_landmarks in results_flip.multi_hand_landmarks:
+
+                fingers = {
+                    'filename' : file
+                }
+
+                for i, finger in enumerate(hand_landmarks.landmark, start=1):
+                    fingers[f'{i}x'] = (finger.x)
+                    fingers[f'{i}y'] = (finger.y)
+                    fingers[f'{i}z'] = (finger.z)
+
+
+                hands_list.append(fingers)
+
+            for hand_landmarks in results_up.multi_hand_landmarks:
+
+                fingers = {
+                    'filename' : file
+                }
+
+                for i, finger in enumerate(hand_landmarks.landmark, start=1):
+                    fingers[f'{i}x'] = (finger.x)
+                    fingers[f'{i}y'] = (finger.y)
+                    fingers[f'{i}z'] = (finger.z)
+
+                hands_list.append(fingers)
+
+            for hand_landmarks in results_down.multi_hand_landmarks:
+
+                fingers = {
+                    'filename' : file
+                }
+
+                for i, finger in enumerate(hand_landmarks.landmark, start=1):
+                    fingers[f'{i}x'] = (finger.x)
+                    fingers[f'{i}y'] = (finger.y)
+                    fingers[f'{i}z'] = (finger.z)
+
+                hands_list.append(fingers)
+
+
     hand_df = pd.DataFrame(hands_list)
     hand_df['target'] = 0
-    hand_df.shape
 
     # if save:
     #     hand_df.to_csv(os.path.join(LOCAL_DATA_PATH,{hand},'_df.csv'), index=False)
@@ -55,7 +119,7 @@ def from_png_to_df(hand: str, verbose=True, save=True) -> pd.DataFrame:
     return hand_df
 
 
-def save_data(data: pd.DataFrame, is_first: bool):
+def save_data(data: pd.DataFrame, hand: str, is_first: bool):
     """
     save the dataset to local disk
     """
@@ -70,3 +134,7 @@ def save_data(data: pd.DataFrame, is_first: bool):
                 index=False)
 
     data.to_csv(os.path.join(LOCAL_DATA_PATH,{hand},'_df.csv'), index=False)
+
+
+df = from_png_to_csv('rock')
+df.head()
--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='chifoumy',
-      version="1.0",
-      description="Project Description",
+      version="0.1",
+      description="Chifoumi Model Detection",
+      author="Chifouteam",
+      author_email="gentil.stephanie@gmail.com",
+      url="https://github.com/ElainePentium/chifoumy",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/chifoumy-run'],
+      # scripts=['scripts/chifoumy-run'],
       zip_safe=False)
