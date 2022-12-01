fefdff717f4a8b5482afbf8a13edc78ffb7d7a7c
david rengifo <argau13@gmail.com>
2022-12-01 17:14:52 +0100

    pipeline with normalization on augmented dataset

---
+++ test_david/test_pierre/Accueil.py
@@ -0,0 +1,34 @@
+#===============================================================================
+
+import matplotlib.pyplot as plt
+import streamlit as st
+import mediapipe as mp
+import pandas as pd
+from PIL import Image
+import numpy as np
+import random
+import os
+import cv2
+from PIL import Image
+import include
+from include import create_key
+
+#===============================================================================
+
+IMAGE_PATH = "../images/"
+
+#===============================================================================
+
+html_title = "<h1 style='color:#FF036A;text-align:left;font-size:90px'>Projet Chifoumy</h1>"
+st.markdown(html_title, unsafe_allow_html=True)
+image_path = IMAGE_PATH + "chifoumi__ter.jpg"
+chifoumi_image = Image.open(image_path)
+picture = st.image(chifoumi_image, width=600)
+#button_play = st.button("Jouer")
+
+#===============================================================================
+
+#st.write(create_key())
+#st.write(create_key())
+#st.write(create_key())
+#st.write(create_key())
